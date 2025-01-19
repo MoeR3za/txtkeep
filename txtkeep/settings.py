eff8eab7cacff061a38ccc6cdfbcbc76ef844003
MoeR3za <666mmCannon@gmail.com>
2025-01-19 20:37:28 +0200

    serve static files with whitenoise

--- txtkeep/settings.py
+++ txtkeep/settings.py
@@ -66,6 +66,7 @@ AWS_S3_FILE_OVERWRITE = False  # Prevent overwriting files with the same name
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -156,7 +157,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Directory for collected static files
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # Default primary key field type
@@ -212,7 +213,7 @@ STORAGES = {
         # 'AWS_S3_SIGNATURE_VERSION': AWS_S3_SIGNATURE_VERSION,
     },
     'staticfiles': {
-        'BACKEND': 'django.contrib.staticfiles.storage.StaticFilesStorage',
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
     },
     'media': {
         'BACKEND': 'storages.backends.s3boto3.S3Boto3Storage',
